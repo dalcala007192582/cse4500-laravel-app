0a6306e6cd11e98998449491c7fbe2531382df84
da2814702 <51937975+da2814702@users.noreply.github.com>
2022-03-02 18:42:17 -0800

    web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/db-test', function() {
+    try {
+	  \DB::connection()->getPDO();
+	  $db_name = \DB::connection()->getDatabaseName();
+	  echo 'Database Connected: '.$db_name;
+    } catch (\Exception $e) {
+	    echo 'None';
+    }
+});
