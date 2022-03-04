892d134ba19695ce0ae0bb6637eab7f7d08b18e0
da2814702 <51937975+da2814702@users.noreply.github.com>
2022-03-03 17:59:45 -0800

    sa

--- resources/views/events-feed.blade.php
+++ resources/views/events-feed.blade.php
@@ -5,4 +5,6 @@ $json_file = file_get_contents('events-feed.JSON');
 
 // Decode the JSON file
 $json_data = json_decode($json_file,true);
+
+return $json_data
 ?>
--- resources/views/events-feed.blade.php.bak
+++ resources/views/events-feed.blade.php.bak
@@ -5,5 +5,4 @@ $json_file = file_get_contents('events-feed.JSON');
 
 // Decode the JSON file
 $json_data = json_decode($json_file,true);
-
 ?>
