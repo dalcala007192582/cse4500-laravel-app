bd8e63a8ab1235b68caa761f0518ecdbdae6eb1a
da2814702 <51937975+da2814702@users.noreply.github.com>
2022-03-11 17:38:19 -0800

    step6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -52,3 +53,5 @@ Route::get('/db-migrate', function() {
 Route::fallback(function() {
 	return view('fallback');
 });
+
+Route::resource('/todos', TodoController::class);
